a65a472ad1e82fa15ab6660d3786717c12b2f210
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-27 08:54:10 -0800

    add nodemon and express for server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('server running...');
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+})
