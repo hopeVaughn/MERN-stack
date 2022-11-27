0fc2bfeae364c44f0dd12130feaca44e9baf2eb2
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-27 07:47:29 -0800

    add form row component to register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -30,7 +30,23 @@ const Register = () => {
         <Logo />
         <h3>Login</h3>
         {/* name input */}
-        <FormRow />
+        <FormRow
+          type='text'
+          name='name'
+          value={values.email}
+          handleChange={handleChange} />
+        {/* email input */}
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange} />
+        {/* password input */}
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange} />
         <button type='submit' className='btn btn-block'>Submit</button>
       </form>
     </Wrapper>
