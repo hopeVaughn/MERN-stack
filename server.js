06b4ebdcbf3e27bcd530ea79a86b2a561e05eb2d
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-28 07:26:13 -0800

    add dot env dependency and add error middleware

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 import express from 'express';
 const app = express();
-
+import dotenv from 'dotenv'
+dotenv.config()
 // middleware
 import notFoundMiddleware from './middleware/not-found.js';
-
-
+import errorHandlerMiddleware from './middleware/error-handler.js';
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
 app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
 const port = process.env.PORT || 5000;
 
 
