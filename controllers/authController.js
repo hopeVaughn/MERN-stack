e73ccc7c0cb7f39cc76a1736b84cec0d4d8943d4
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-29 08:11:47 -0800

    add specific error messages for auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,13 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
 const register = async (req, res) => {
+  const { name, email, password } = req.body;
 
-  const user = await User.create(req.body);
+  if (!name || !email || !password) {
+    throw new Error('please provide all values');
+  }
+
+  const user = await User.create(name, email, password);
   res.status(StatusCodes.CREATED).json({ user })
 
 }
