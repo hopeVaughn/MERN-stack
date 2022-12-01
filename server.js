76b3971344614b2318b27d3e153bd26de927caf3
hopeVaughn <hopevaughnwarren@gmail.com>
2022-12-01 08:06:41 -0800

    add morgan dependency

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const app = express();
-import dotenv from 'dotenv'
-dotenv.config()
-import 'express-async-errors'
-
+import dotenv from 'dotenv';
+dotenv.config();
+import 'express-async-errors';
+import morgan from 'morgan';
 // db and authenticateUser
 import connectDB from './db/connect.js';
 
@@ -15,6 +15,9 @@ import jobsRouter from './routes/jobsRoutes.js'
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'))
+}
 app.use(express.json());
 
 // app.get('/', (req, res) => {
