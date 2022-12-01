4ffc0f36fbca3b39074dcd75a7fe44602421fb21
hopeVaughn <hopevaughnwarren@gmail.com>
2022-12-01 09:03:11 -0800

    complete front end login functionality

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -26,6 +26,7 @@ const reducer = (state, action) => {
       alertText: '',
     }
   }
+  // register user state changes
   if (action.type === REGISTER_USER_BEGIN) {
     return { ...state, isLoading: true }
   }
@@ -51,6 +52,34 @@ const reducer = (state, action) => {
       alertText: action.payload.msg,
     }
   }
+  // end of register state changes
+  // login user state changes
+  if (action.type === LOGIN_USER_BEGIN) {
+    return { ...state, isLoading: true }
+  }
+  if (action.type === LOGIN_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      token: action.payload.token,
+      user: action.payload.user,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+      showAlert: true,
+      alertType: 'success',
+      alertText: 'Login Successful! Redirecting...',
+    }
+  }
+  if (action.type === LOGIN_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'danger',
+      alertText: action.payload.msg,
+    }
+  }
+  //end of login user state changes
 
   throw new Error(`no such action: ${action.type}`)
 }
