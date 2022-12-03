918cd0e8057c16adfe0c36ded1a9cc05e973b1d9
hopeVaughn <hopevaughnwarren@gmail.com>
2022-12-03 08:15:14 -0800

    add shared layout components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,10 +1,14 @@
 import Logo from "./Logo";
 import FormRow from "./FormRow";
 import Alert from "./Alert";
-
-
+import Navbar from "./Navbar";
+import SmallSidebar from "./SmallSidebar";
+import BigSidebar from "./BigSidebar";
 export {
+  BigSidebar,
   Logo,
   FormRow,
-  Alert
+  Alert,
+  SmallSidebar,
+  Navbar
 };
