0844f350bf6c1fafc9a6e743dca0e16f5af6418f
hopeVaughn <hopevaughnwarren@gmail.com>
2022-12-07 11:51:27 -0800

    set up instance using axios library

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -32,7 +32,13 @@ const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-
+  //axios default
+  const authFetch = axios.create({
+    baseURL: '/api/v1',
+    headers: {
+      Authorization: `Bearer ${state.token}`,
+    }
+  })
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT });
     clearAlert();
@@ -87,19 +93,12 @@ const AppProvider = ({ children }) => {
 
   const updateUser = async (currentUser) => {
     try {
-      const { data } = await axios.patch('/api/v1/auth/updateUser', currentUser, {
-        headers: {
-          Authorization: `Bearer ${state.token}`,
-        },
-      });
+      const { data } = await authFetch.patch('/auth/updateUser', currentUser);
       console.log(data);
     } catch (error) {
       console.log(error.response);
     }
   };
-
-
-
 
   const values = {
     ...state,
