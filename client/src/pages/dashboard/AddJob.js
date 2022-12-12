e4fedbd96ec3936c7bcef8b8e947dcd349ad0997
hopeVaughn <hopevaughnwarren@gmail.com>
2022-12-12 08:20:48 -0800

    add job type options basic setup

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -63,6 +63,28 @@ const AddJob = () => {
             handleChange={handleJobInput}
           />
           {/* job type */}
+          <div className="form-row">
+            <label
+              htmlFor="jobType"
+              className='form-label'>
+              job type
+            </label>
+            <select
+              name="jobType"
+              value={jobType}
+              onChange={handleJobInput}
+              className="form-select"
+            >
+              {jobTypeOptions.map((itemValue, index) => {
+                return <option
+                  key={index}
+                  value={itemValue}
+                >
+                  {itemValue}
+                </option>
+              })}
+            </select>
+          </div>
           {/* job status */}
           <div className="button-container">
             <button
