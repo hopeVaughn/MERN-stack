288f22858e155e4784f1ef4fe19ce10b4712c117
hopeVaughn <hopevaughnwarren@gmail.com>
2022-12-12 08:30:27 -0800

    add form select component then add to add job page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,7 +4,9 @@ import Alert from "./Alert";
 import Navbar from "./Navbar";
 import SmallSidebar from "./SmallSidebar";
 import BigSidebar from "./BigSidebar";
+import FormRowSelect from "./FormRowSelect";
 export {
+  FormRowSelect,
   BigSidebar,
   Logo,
   FormRow,
