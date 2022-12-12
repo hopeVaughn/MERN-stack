c398c116e8a1856cea26dcf1a80e266abd2e1e90
hopeVaughn <hopevaughnwarren@gmail.com>
2022-12-12 08:13:00 -0800

    add main structure to add job page

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -1,6 +1,81 @@
+import { FormRow, Alert } from '../../components';
+import { useAppContext } from '../../context/appContext';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+
 const AddJob = () => {
+  const {
+    isEditing,
+    showAlert,
+    displayAlert,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+  } = useAppContext();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!position || !company || !jobLocation) {
+      displayAlert();
+      return
+    }
+    console.log('create job');
+  }
+
+  const handleJobInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    console.log(`${name}:${value}`);
+
+  }
+
   return (
-    <h1>AddJob Page</h1>
+    <Wrapper>
+      <form className='form'>
+        <h3>{isEditing ? 'edit job' : 'add job'}</h3>
+        {showAlert && <Alert />}
+        <div className="form-center">
+          {/* position */}
+          <FormRow
+            type="text"
+            name="position"
+            value={position}
+            handleChange={handleJobInput}
+          />
+          {/* company */}
+          <FormRow
+            type="text"
+            name="company"
+            value={company}
+            handleChange={handleJobInput}
+          />
+          {/* location */}
+          <FormRow
+            type="text"
+            labelText='job location'
+            name="jobLocation"
+            value={jobLocation}
+            handleChange={handleJobInput}
+          />
+          {/* job type */}
+          {/* job status */}
+          <div className="button-container">
+            <button
+              type="submit"
+              className='btn btn-block submit-btn'
+              onClick={handleSubmit}
+            >
+              submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
   )
 }
 export default AddJob
