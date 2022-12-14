decb6240f0d3dd3fe3d504fd64a1528414b1e9a2
hopeVaughn <hopevaughnwarren@gmail.com>
2022-12-14 09:07:18 -0800

    add server functionality for get all jobs

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -14,7 +14,13 @@ const createJob = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ job });
 }
 const getAllJobs = async (req, res) => {
-  res.send('get All Jobs')
+  const jobs = await Job.find({ createdBy: req.user.userId })
+  res.status(StatusCodes.OK)
+    .json({
+      jobs,
+      totalJobs: jobs.length,
+      numOfPages: 1
+    });
 }
 const updateJob = async (req, res) => {
   res.send('update Job')
