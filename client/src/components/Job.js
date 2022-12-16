f04b994b471aa9e7869c13986b40a36c033b4fe3
hopeVaughn <hopevaughnwarren@gmail.com>
2022-12-16 08:41:10 -0800

    update all job component with moment.js

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,6 +1,13 @@
-const Job = ({ company }) => {
+import moment from 'moment'
+
+const Job = ({ company, createdAt }) => {
+  let date = moment(createdAt);
+  date = date.format('MMM Do, YYYY')
   return (
-    <h5>{company}</h5>
+    <div className="">
+      <h5>{company}</h5>
+      <h5>{date}</h5>
+    </div>
   )
 }
 export default Job
