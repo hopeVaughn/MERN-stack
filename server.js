711028fb4874b15590a12b0aadceeea7c2d5bed2
hopeVaughn <hopevaughnwarren@gmail.com>
2022-12-20 08:43:46 -0800

    add security packages to server

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 import 'express-async-errors';
 import morgan from 'morgan';
+
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import path from 'path';
+
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
 
 // db and authenticateUser
 import connectDB from './db/connect.js';
@@ -27,7 +32,12 @@ if (process.env.NODE_ENV !== 'production') {
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(path.resolve(__dirname, './client/build')));
+
 app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
